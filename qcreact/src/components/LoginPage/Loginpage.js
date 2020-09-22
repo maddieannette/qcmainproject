8a87e3f5bd546caf71df679a340686dbfb3de429
maddieannette <madeline.a.flores@gmail.com>
2020-09-21 21:36:17 -0500

    Altered Login page to material ui and added image

--- qcreact/src/components/LoginPage/Loginpage.js
+++ qcreact/src/components/LoginPage/Loginpage.js
@@ -1,57 +1,118 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LocalHospitalRoundedIcon from '@material-ui/icons/LocalHospitalRounded';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
 
-class Login extends Component {
-    submitForm(e) {
-        e.preventDefault();
-        this.props.history.push('./DashBoard');
-    }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/qI7USKbZY_A)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-    render() {
-        return (
-            <form onSubmit={this.submitForm.bind(this)} className="auth-inner">
-                <h3>Log In</h3>
+export default function Login() {
+  const classes = useStyles();
 
-                <div className="form-group">
-                    <label htmlFor="userInput">Username
-                        <input
-                            id="userInput"
-                            type="username"
-                            className="form-control"
-                            placeholder="Enter username"
-                            required
-                        />
-                    </label>
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="passwordInput">Password
-                        <input
-                            id="passwordInput"
-                            type="password"
-                            className="form-control"
-                            placeholder="Enter password"
-                            required
-                        />
-                    </label>
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <label className="custom-control-label" htmlFor="customCheck1">
-                            <input
-                                type="checkbox"
-                                className="custom-control-input"
-                                id="customCheck1"
-                            />
-                            Remember me
-                        </label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-            </form>
-        );
-    }
+  return (
+    <Grid container  className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar color="green" className={classes.avatar}>
+            <LocalHospitalRoundedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Lo
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
 }
-
-export default Login;
